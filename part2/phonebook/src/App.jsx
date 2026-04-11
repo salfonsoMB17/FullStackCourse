3a2ab75ad7c415eba1f2dbef32fc8def9d58ded3
salfonsoMB17 <serggios17@gmail.com>
2026-04-11 16:18:19 -0500

    Exercise 2.8: Add phone number input and display functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,13 +3,19 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -21,24 +27,32 @@ const App = () => {
     }
 
     const personObject = {
+      number: newNumber,
       name: newName,
       id: persons.length + 1,
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>debug: {newName}</div>
+        <div>debug: {newName} {newNumber}</div>
         <div>
           name: <input
           value={newName}
           onChange={handleNameChange}
         />        
+        </div>
+        <div>number: 
+          <input 
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
         </div>
         <div>
           <button type="submit">add</button>
