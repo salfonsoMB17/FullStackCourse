a0c5ca298330ccc87abf56649eb2774c1be3c192
salfonsoMB17 <serggios17@gmail.com>
2026-04-11 15:58:16 -0500

    Exercise 2.6: Add phonebook with name input functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,47 @@
-import Note from './components/Note'
+import { useState } from 'react'
+import Person from './components/Person'
 
-const App = ({ notes }) => {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>debug: {newName}</div>
+        <div>
+          name: <input
+          value={newName}
+          onChange={handleNameChange}
+        />        
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
       <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
+        {persons.map((person) => (
+          <Person key={person.id} person={person} />
         ))}
       </ul>
     </div>
